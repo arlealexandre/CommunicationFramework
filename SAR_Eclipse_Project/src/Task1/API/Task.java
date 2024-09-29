f4b47462e18c3e3d6f72b9583b53026684b24ebe
arlealexandre <arle.alexandre@protonmail.com>
2024-09-29 14:18:13 +0200

    adding task implementation

--- SAR_Eclipse_Project/src/Task1/API/Task.java
+++ SAR_Eclipse_Project/src/Task1/API/Task.java
@@ -1,12 +1,19 @@
 package Task1.API;
 
 public abstract class Task extends Thread {
+	
+	private Broker broker;
+	protected Runnable runnable;
 
 	public Task(Broker b, Runnable r) {
-		
+		this.broker = b;
+		this.runnable = r;
 	}
 	
 	public static Broker getBroker() {
+		if (Thread.currentThread() instanceof Task) {
+			return ((Task) Thread.currentThread()).broker;
+		}
 		return null;
 	}
 }
--- SAR_Eclipse_Project/src/Task1/Implementation/TaskImpl.java
+++ SAR_Eclipse_Project/src/Task1/Implementation/TaskImpl.java
@@ -4,9 +4,17 @@ import Task1.API.Broker;
 import Task1.API.Task;
 
 public class TaskImpl extends Task {
-
+	
 	public TaskImpl(Broker b, Runnable r) {
 		super(b, r);
 	}
 
+	@Override
+	public void run() {
+		if (this.runnable != null) {
+			runnable.run();
+		} else {
+			throw new NullPointerException("Runnable is null.");
+		}
+	}
 }
